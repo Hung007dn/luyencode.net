8ccd0d6f2eb7b3b13d8aa022a307feda907bfa7b
anh ba hung <vutronghung1704.qtr@gmail.com>
2026-07-20 10:52:10 +0700

    bài giải luyencode
    Implement rectangle perimeter and area calculation.

---
+++ CB06_tinhchuvidientich_hcn.cpp
@@ -0,0 +1,12 @@
+//C++
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int d,r;
+    cin>>d>>r;
+   int chuvi=(d+r)*2;
+   int dientich=d*r;
+   cout<<chuvi<<endl;
+   cout<<dientich;
+    return 0;
+}
