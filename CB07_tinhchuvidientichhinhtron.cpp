c3f1fee0a9894e86eda1ab1d2d17124a7f01f4e4
anh ba hung <vutronghung1704.qtr@gmail.com>
2026-07-20 10:53:03 +0700

    bài giải luyencode
    Implement a program to calculate the circumference and area of a circle based on user input.

---
+++ CB07_tinhchuvidientichhinhtron.cpp
@@ -0,0 +1,12 @@
+//C++
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   double r;
+   cin>>r;
+   double pi= 3.14;
+   double chuvi=2*r*pi;
+   double dientich= r*r*pi;
+   cout<<fixed<<setprecision(3)<<chuvi<<" "<<dientich;
+    return 0;
+}
