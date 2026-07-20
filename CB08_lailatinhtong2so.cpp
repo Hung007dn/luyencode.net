6039b3208766eaee520e7d327edf52cb3b34994f
anh ba hung <vutronghung1704.qtr@gmail.com>
2026-07-20 10:54:13 +0700

    bài giải luyencode

---
+++ CB08_lailatinhtong2so.cpp
@@ -0,0 +1,10 @@
+//C++
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b;
+  cin>>a>>b;
+  int ketqua=a+b;
+  cout<<a<<" "<<'+'<<" "<<b<<" "<<'='<<" "<<ketqua;
+    return 0;
+}
