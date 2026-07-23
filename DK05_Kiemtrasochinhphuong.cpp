29608629dc240d8f839f5503ebd5197ff5925d96
anh ba hung <vutronghung1704.qtr@gmail.com>
2026-07-23 20:43:51 +0700

    bài giải luyencode

---
+++ DK05_Kiemtrasochinhphuong.cpp
@@ -0,0 +1,15 @@
+//C++
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    long long n,a;
+    cin >> n;
+    a = sqrt(n);
+    if (a * a == n){
+        cout << "YES";
+    }else{
+        cout << "NO";
+    }
+}
