5abb0e7a7ffbfbf44e862b161356936e8af2221c
anh ba hung <vutronghung1704.qtr@gmail.com>
2026-07-23 20:47:53 +0700

    bài giải luyencode

---
+++ DK07_giaiphuongtrinh.cpp
@@ -0,0 +1,40 @@
+//C++
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a == 0) {
+        if (b == 0) {
+            if (c == 0) {
+                cout << "WOW"; // vô số nghiệm
+            } else {
+                cout << "NO";  // vô nghiệm
+            }
+        } else {
+            double x = -1.0 * c / b;
+            cout << fixed << setprecision(2) << x;
+        }
+    } 
+    else {
+        double delta = b * b - 4 * a * c;
+        if (delta < 0) {
+            cout << "NO";
+        } 
+        else if (delta == 0) {
+            double x = -b / (2.0 * a);
+            cout << fixed << setprecision(2) << x;
+        } 
+        else {
+            double x1 = (-b + sqrt(delta)) / (2.0 * a);
+            double x2 = (-b - sqrt(delta)) / (2.0 * a);
+            if (x1 > x2) swap(x1, x2);
+            cout << fixed << setprecision(2) << x1 << " " << x2;
+        }
+    }
+
+    return 0;
+}
