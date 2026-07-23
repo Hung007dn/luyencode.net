7fe3383692b8a425b9c89de3af22f0d60985f259
anh ba hung <vutronghung1704.qtr@gmail.com>
2026-07-23 21:20:02 +0700

    bài giải luyencode
    Implement a program to print numbers from a to b.

---
+++ VL01_in ra các số từ a đến b.cpp
@@ -0,0 +1,14 @@
+//C++
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  long long a,b;
+  cin>>a>>b;
+  int s[1000];
+  for(int i=a;i<=b;i++)
+  {
+      cout<<i<<" ";// chỉ in các phần tử (là i) không in ra một dãy s[i];
+  }
+    return 0;
+}
